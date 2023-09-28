f438e4a4335c9c310ccecb4ef335fb4ef3a54f19
Quin Nordmark <47016826+n0rdie@users.noreply.github.com>
2023-09-28 16:50:30 -0700

    Final submission

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -6,23 +6,29 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+puts "Hello World".downcase
+# Returns string with the input text but all lowercase letters. 
+# Would return "hello world"
 
+puts "Hello World".include?("Hello")
+# Returns a true/false bool depending on weather the given string has the other string.
+# Would return true.
 
-"Hello World".include?("Hello")
+puts "Hello World".end_with?("Hello")
+# Returns bool true/false depending on the last characters of the first string's last characters having the same characters as the second string.
+# Would return false.
 
+puts "Hello World".end_with?("rld")
+# Returns bool true/false depending on the last characters of the first string's last characters having the same characters as the second string.
+# Would return true.
 
-"Hello World".end_with?("Hello")
+puts 12.even?
+# Checks to see if dividing by 2 would give a whole number.
+# Would return true.
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
+puts 18.next
+# Checks to see if dividing by 2 would return a whole number
+# Would return true.
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -39,6 +45,18 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+# gsub returns the same string as input however replaces all characters in the first slot with the cahracter in the second
+# would return K*rd*sh**n
+last_name = "Kardashian"
+puts last_name.gsub(/[aeiou]/, '*')
+
+# empty? returns true if the length of the string is 0, false if not
+# would return true for the first, false for the second.
+empty_string = ""
+puts empty_string.empty?
+full_string = "McDonalds"
+puts full_string.empty?
+
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -48,9 +66,29 @@ puts first_name.start_with?("J")
 # Include comments above each method call explaining the impact and return value of that method.
 
 
+# pow returns the first number to the power of the second number
+# returns 243
+three = 3
+puts three.pow(5)
+
+# fdiv divides the first number by the second number and returns a float
+eight = 8
+puts eight.fdiv(2)
+
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+
+# compact deletes all nil from the array and puts all remaining variables next to each other
+# returns [1,3,5]
+arr = [1, nil, 3, nil, 5]
+puts arr.compact
+
+# uniq deletes all indexes that have the same value of an index that has come before
+# returns [1,2]
+arr = [1,1,1,1,1,1,1,2]
+puts arr.uniq
