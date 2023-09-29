154e6d474d8ff24f358b8baab63687429db270de
Quin Nordmark <47016826+n0rdie@users.noreply.github.com>
2023-09-28 17:12:13 -0700

    Completed_assignment

--- final_practice.rb
+++ final_practice.rb
@@ -6,43 +6,78 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method? - A string saying "hi"
+# How many arguments did you pass your method? - No arguements were added.
+
+
+def greeting
+    return "hi"
+end
+puts greeting
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? - "hi Quin"
+# How many arguments did you pass your method? - A simple name
+# What data type was your argument(s)? - String
+
+
+def custom_greeting (name)
+    return_string = "hi " + name.to_s
+    return return_string
+end
+puts custom_greeting("Quin")
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? - "hi freddy fazbear pizza"
+# How many arguments did you pass your method? - Three strings
+# What data type was your argument(s)? - All Strings
 
+
+def greet_person (first,middle,last)
+    return "hi " + first + " " + middle + " " + last
+end
+puts greet_person("freddy", "fazbear", "pizza")
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? - "squared 100 = 10000"
+# How many arguments did you pass your method? - Just one
+# What data type was your argument(s)? - int
+
+
+def square (num)
+    squared = num*num
+    return "squared " + num.to_s + " = " + squared.to_s
+end
+puts square(100)
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+def check_stock(num, item)
+    if num > 3
+        return item + " is stocked"
+    elsif num > 0
+        return item + " running low"
+    else
+        return item + "out of stock"
+    end
+end
+
+puts check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+puts check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+puts check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+puts check_stock(1, "Salsa");
 # => "Salsa - running LOW"
